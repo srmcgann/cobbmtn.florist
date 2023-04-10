1f1716269c04af80c9adb196fdaca458c3c32155
Scott Mc Gann <s.r.mcgann@hotmail.com>
2023-04-10 10:44:00 -0700

    sync

--- static/db.php
+++ static/db.php
@@ -1,6 +1,6 @@
 <?
   $user = 'user';
-  $pass = 'chrome57253';
+  $pass=explode("\n", file_get_contents('/home/cantelope/plorgpw'))[0];
   $db = 'cobbmtn.new';
   $host = 'localhost';
   $link = mysqli_connect($host, $user, $pass, $db);
